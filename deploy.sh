291758b28ed4354aec508653f9d23f4b9ab8674b
zippolee <kelou322@gmail.com>
2019-07-30 01:30:47 +0800

    修改发布分支为master

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # 生成静态文件
-npm run docs:build
+cnpm run docs:build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:ZippoLee12138/ZippoLee12138.github.io.git master:gh-pages
+git push -f git@github.com:ZippoLee12138/ZippoLee12138.github.io.git master:master
 
 cd -
